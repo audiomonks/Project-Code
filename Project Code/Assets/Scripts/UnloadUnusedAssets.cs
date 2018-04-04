e5a7b002cd4c1a1fa8dafa3ba9228f4e1030e7dc
audiomonks <jeramibellofatto@gmail.com>
2018-04-04 00:42:15 -0700

    April 4th
    Working:
    Grab and move
    Collecting objects
    Not Working:
    Object placed on console.
    TODO:
    Objects snap to recieving object
    Fix Raycast so it is only cast when mouse clicked. Not on every frame

---
+++ Project Code/Assets/Scripts/UnloadUnusedAssets.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadUnusedAssets : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #if UNITY_EDITOR
+
+    [UnityEditor.MenuItem("Assets/Unload Assets")]
+    static void UnloadAssets()
+    {
+        Resources.UnloadUnusedAssets();
+    }
+
+    #endif
+}
